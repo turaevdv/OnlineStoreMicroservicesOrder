a777dff578bf13857e1dbe99aab3552eeb0e8dea
Turaev <denis.turaev@bk.ru>
2022-08-22 09:22:36 +0300

    Added logic for sending orders to the Report microservice

--- src/main/java/ru/turaev/order/repository/OrderRepository.java
+++ src/main/java/ru/turaev/order/repository/OrderRepository.java
@@ -2,6 +2,7 @@ package ru.turaev.order.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 import ru.turaev.order.model.Order;
 
@@ -13,5 +14,5 @@ public interface OrderRepository extends JpaRepository<Order, Long> {
     List<Order> getAllByIsCanceledIsFalse();
 
     @Query("select o from Order o where o.pickupPointId = :id and o.orderDate between :begin and :end and o.isCanceled = false ORDER BY o.orderDate")
-    List<Order> getAllNonCancelledOrdersOfPickupPointByPeriod(long id, LocalDate begin, LocalDate end);
+    List<Order> getAllNonCancelledOrdersOfPickupPointByPeriod(@Param("id") long id, @Param("begin") LocalDate begin, @Param("end") LocalDate end);
 }
